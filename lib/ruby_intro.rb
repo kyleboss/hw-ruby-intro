af2dc7046238bf8323d0577cf92129bc0acd2f81
Kyle Boss <kyle@kyleboss.com>
2016-01-27 23:21:10 +0000

    Finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,59 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  current_total = 0
+  arr.each do |current_int|
+    current_total += current_int
+  end
+  current_total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.length == 0
+  return arr.first if arr.length == 1
+  arr.sort!
+  arr[-1] + arr[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.length == 1
+  num_iterations = (arr.length/2).ceil
+  arr[0..num_iterations].each do |curr_outer_int|
+    arr.each do |curr_inner_int|
+      return true if (curr_inner_int + curr_outer_int == n)
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^(?=\w)([^?=aeiou])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+  dec_s = s.to_i(10)
+  return dec_s % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :price, :isbn
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$" + '%.2f' % price
+  end
 end
